7e185061f2caba5e7f33b55f6ccf0e6c4797a613
PEP <borrellpep@gmail.com>
2019-05-02 00:00:03 +0200

    Primer commit. Començament del treball.

---
+++ data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from pandas import DataFrame
+from haversine import haversine
+from geopy.geocoders import Nominatim
+import networkx as nx
+from staticmap import StaticMap, CircleMarker, Line
+
+def get_nodes():
+    url = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
+    bicing = DataFrame.from_records(pd.read_json(url)['data']['stations'], index='station_id')
+    G = nx.Graph()
+    for st in bicing.itertuples():
+        G.add_node(st.Index, lat=st.lat, lon=st.lon)
+    return G
+
+def number_of_nodes(G):
+    return G.number_of_nodes()
+
+def number_of_edges(G):
+    return G.number_of_edges()
+
+def plot_graph(G):
+    m_bcn = StaticMap(800, 800)
+    for node in list(G.nodes(data=True)):
+        marker = CircleMarker((node[1]['lon'], node[1]['lat']), 'red', 4)
+        m_bcn.add_marker(marker)
+    for edge in list(G.edges()):
+        line = Line(coords, 'blue', 3)
+        m_bcn.add_line(line)
+    image = m_bcn.render()
+    return image
---
+++ test.py
@@ -0,0 +1,7 @@
+from data import get_nodes, number_of_nodes, number_of_edges, plot_graph
+
+G = get_nodes()
+print(number_of_nodes(G))
+print(number_of_edges(G))
+image = plot_graph(G)
+image.save('map.png')
