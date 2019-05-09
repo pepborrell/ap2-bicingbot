94f8f56570dcb2036b68bf915b186f7b62f846be
Josep Borrell Tatche <josep.borrell@c6s302pc42.fib.upc.es>
2019-05-09 11:07:41 +0200

    Alguna millora

--- data.py
+++ data.py
@@ -5,6 +5,10 @@ from geopy.geocoders import Nominatim
 import networkx as nx
 from staticmap import StaticMap, CircleMarker, Line
 
+"""Descarrega les dades de les estacions d'internet i les situa en un Graph de NetworkX
+Retorna el graf, amb les dades d'índex i posició a cada node.
+Les dades de posició són una tupla construïda com (longitud, latitud).
+"""
 def get_nodes():
     url = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
     bicing = DataFrame.from_records(pd.read_json(url)['data']['stations'], index='station_id')
@@ -20,13 +24,16 @@ def number_of_nodes(G):
 def number_of_edges(G):
     return G.number_of_edges()
 
+"""Pinta el graf a un mapa.
+Pren com a argument un Graph de NetworkX, primer en pinta els nodes i després les arestes.
+"""
 def plot_graph(G):
     m_bcn = StaticMap(800, 800)
     for node in list(G.nodes(data=True)):
         marker = CircleMarker(node[1]['pos'], 'red', 4)
         m_bcn.add_marker(marker)
     for edge in list(G.edges()):
-        line = Line(coords, 'blue', 3)
+        line = Line(G.nodes[edge[0]]['pos'], G.nodes[edge[1]]['pos'], 'blue', 3)
         m_bcn.add_line(line)
     image = m_bcn.render()
     return image
