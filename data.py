c8c2289d6cecee2bf45d1816b939a5423a3494f7
PEP <borrellpep@gmail.com>
2019-05-13 20:43:34 +0200

    Comentaris en anglès. Funció del Jordi afegida.

--- data.py
+++ data.py
@@ -5,9 +5,11 @@ from geopy.geocoders import Nominatim
 import networkx as nx
 from staticmap import StaticMap, CircleMarker, Line
 
-"""Descarrega les dades de les estacions d'internet i les situa en un Graph de NetworkX
-Retorna el graf, amb les dades d'índex i posició a cada node.
-Les dades de posició són una tupla construïda com (longitud, latitud).
+"""
+Downloads the data from the internet and places the stations in a NetworkX Graph.
+Returns the graph, with the index and position in the node's own data and the
+DataFrame containing all the downloaded data.
+The position data is a tuple of the form (longitude, latitude)
 """
 def get_nodes():
     url = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
@@ -16,7 +18,7 @@ def get_nodes():
     for st in bicing.itertuples():
         position = (st.lon, st.lat)
         G.add_node(st.Index, pos=position)
-    return G
+    return G, bicing
 
 def number_of_nodes(G):
     return G.number_of_nodes()
@@ -24,16 +26,46 @@ def number_of_nodes(G):
 def number_of_edges(G):
     return G.number_of_edges()
 
-"""Pinta el graf a un mapa.
-Pren com a argument un Graph de NetworkX, primer en pinta els nodes i després les arestes.
+"""
+Plots the graph as a map, using the coordinates of the nodes.
+Takes a NetworkX Graph and returns the image
 """
 def plot_graph(G):
-    m_bcn = StaticMap(800, 800)
+    map = StaticMap(800, 800)
+    # Plotting nodes
     for node in list(G.nodes(data=True)):
         marker = CircleMarker(node[1]['pos'], 'red', 4)
-        m_bcn.add_marker(marker)
+        map.add_marker(marker)
+    # Plotting edges
     for edge in list(G.edges()):
         line = Line(G.nodes[edge[0]]['pos'], G.nodes[edge[1]]['pos'], 'blue', 3)
-        m_bcn.add_line(line)
-    image = m_bcn.render()
+        map.add_line(line)
+    image = map.render()
     return image
+
+def addressesTOcoordinates(addresses):
+    '''
+    Returns the two coordinates of two addresses of Barcelona
+    in a single string separated by a comma. In case of failure, returns None.
+
+    Examples:
+
+    >>> addressesTOcoordinates('Jordi Girona, Plaça de Sant Jaume')
+    ((41.3875495, 2.113918), (41.38264975, 2.17699121912479))
+    >>> addressesTOcoordinates('Passeig de Gràcia 92, La Rambla 51')
+    ((41.3952564, 2.1615724), (41.38082045, 2.17357087674997))
+    >>> addressesTOcoordinates('Avinguda de Jordi Cortadella, Carrer de Jordi Petit')
+    None
+    >>> addressesTOcoordinates('foo')
+    None
+    >>> addressesTOcoordinates('foo, bar, lol')
+    None
+    '''
+    try:
+        geolocator = Nominatim(user_agent="bicing_bot")
+        address1, address2 = addresses.split(',')
+        location1 = geolocator.geocode(address1 + ', Barcelona')
+        location2 = geolocator.geocode(address2 + ', Barcelona')
+        return (location1.latitude, location1.longitude), (location2.latitude, location2.longitude)
+    except:
+        return None
