1f79035f0a80ae1fdc2efa2b30b5b79368379e67
PEP <borrellpep@gmail.com>
2019-05-15 22:24:42 +0200

    Fallava el test

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from data import get_nodes, number_of_nodes, number_of_edges, plot_graph
 
-G = get_nodes()
+G, bicing = get_nodes()
 print(number_of_nodes(G))
 print(number_of_edges(G))
 image = plot_graph(G)
