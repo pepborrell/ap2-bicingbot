7c154a65ca1fb8a973774eb82287ea41cc5b1bd6
mireiacavalle <50320792+mireiacavalle@users.noreply.github.com>
2019-05-15 23:18:03 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
-from data import get_nodes, number_of_nodes, number_of_edges, plot_graph
+from data import get_nodes, number_of_nodes, number_of_edges, number_of_connected_components, plot_graph
 
 G, bicing = get_nodes()
 print(number_of_nodes(G))
 print(number_of_edges(G))
+print(number_of_connected_components(G))
 image = plot_graph(G)
 image.save('map.png')
